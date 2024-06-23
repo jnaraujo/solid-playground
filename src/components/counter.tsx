9c42c8652a683370ddae8b867a1db0af176a6c1c
Jônatas A. <jonatasaraujo2257@gmail.com>
2024-06-23 14:12:13 -0300

    add stuff

---
+++ src/components/counter.tsx
@@ -0,0 +1,5 @@
+export function Counter() {
+  return <div>
+    <h2>Counter</h2>
+  </div>
+}
---
+++ src/routes/home.tsx
@@ -0,0 +1,7 @@
+import { Counter } from "../components/counter";
+
+export function HomePage() {
+  return <main>
+    <Counter />
+  </main>
+}
